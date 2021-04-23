e2ce698722e334cced8b3a67f9a11dc412751c91
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-04-23 15:07:33 +0800

    update sencore

--- apidocs/source/conf.py
+++ apidocs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Phoenix Grey'
 author = 'Phoenix Grey'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
